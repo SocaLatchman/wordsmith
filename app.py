05eb2d38f0e36c2092811be24166732f208fec38
Soca <soca@Socas-Mac-mini.local>
2025-12-25 21:18:06 -0800

    Project structure and routes

---
+++ app.py
@@ -0,0 +1,71 @@
+from flask import Flask, render_template, session
+from flask_wtf import CSRFProtect
+
+
+app = Flask(__name__)
+csrf = CSRFProtect(app)
+
+
+@app.route('/')
+def index():
+    pass
+
+@app.route('/register')
+def registration():
+    pass
+
+@app.route('/register/passcode')
+def registration_passcode():
+    pass
+
+@app.route('/signin')
+def signin():
+    pass
+
+@app.route('/signin/passcode')
+def signin_passcode():
+    pass
+
+@app.route('/email/passcode')
+def send_passcode():
+    pass
+
+@app.route('/wordbank')
+def wordbank():
+    pass
+
+@app.route('/wordbank/delete/<word_id>')
+def delete_word():
+    pass
+
+@app.route('/wordbank/<word_id>')
+def get_word():
+    pass
+
+@app.route('/wordbank/bookmark/<word_id>')
+def bookmark_word():
+    pass
+
+@app.route('/signout')
+def signout():
+    pass
+
+@app.route('/api/words/<letter>')
+def letter():
+    pass
+
+@app.route('/api/search/<word>')
+def word_search():
+    pass
+
+@app.route('/api/random/word')
+def random_word():
+    pass
+
+@app.route('/api/synonyms/<word>')
+def synonyms():
+    pass
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
