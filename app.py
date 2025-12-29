4140cb41f4c7ac0c8ced1f67cb0b4a8863109185
SocaLatchman <r.latchman3624@gmail.com>
2025-12-28 17:25:19 -0800

    User model send_passcode method and supporting view function

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ from flask import Flask, render_template, session, request, url_for, redirect, j
 from flask_wtf import CSRFProtect
 from typing import List
 from sqlmodel import Field, SQLModel, Column, JSON, create_engine
+from apscheduler.schedulers.background import BackgroundScheduler
+from email_validator import validate_email, EmailNotValidError
 from dotenv import load_dotenv
-from flask_mail import Mail, Message
+from flask_mailman import EmailMessage, Mail, EmailMultiAlternatives
 from datetime import datetime
 from redis import Redis
 import requests
@@ -16,19 +18,40 @@ import time
 load_dotenv('.env')
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['MAIL_SERVER'] = os.environ.get('MAIL_SERVER')
+app.config['MAIL_PORT'] = os.environ.get('MAIL_PORT')
+app.config['MAIL_USE_TLS'] = os.environ.get('MAIL_USE_TLS')
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('MAIL_DEFAULT_SENDER')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
 app.config['DICTIONARY_API'] = os.environ.get('DICTIONARY_API')
 app.config['REDIS_URL'] = os.environ.get('REDIS_URL')
+scheduler = BackgroundScheduler()
 csrf = CSRFProtect(app)
-
+mail = Mail(app)
 engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'], echo=True)
 
 class User(SQLModel, table=True):
     id: int = Field(default=None, primary_key=True)
-    fullname: str
-    email: str 
+    fullname: str = Field(default='anonymous')
+    email: str  = Field(unique=True)
     date_created : datetime
     last_active: datetime
+
+    def send_passcode(email):
+        try:
+             wotd = EmailMultiAlternatives(
+                subject='Passcode', 
+                body=render_template('email.txt', passcode=''),
+                from_email=app.config['MAIL_USERNAME'], 
+                to=[email]
+             )
+             wotd.attach_alternative(render_template('email.html', passcode=''), 'text/html')
+             wotd.send()
+             return 'Message sent'
+        except Exception as e:
+             return f'error: {str(e)}'
 
 class Wordbank(SQLModel, table=True):
     word_id: int = Field(default=None, primary_key=True)
@@ -62,6 +85,10 @@ class Wordbank(SQLModel, table=True):
                         else:
                             return word['synonyms']
 
+    def send_word(email):
+        pass
+
+
 @app.route('/')
 def index():
     return render_template('index.html', title='Get a Word a Day')
@@ -84,7 +111,8 @@ def signin_passcode():
 
 @app.route('/email/passcode')
 def send_passcode():
-    pass
+    send = User.send_passcode('abstractblk@gmail.com')
+    return {'success' : 200}
 
 @app.route('/wordbank')
 def wordbank():
@@ -110,10 +138,6 @@ def signout():
 def docs():
     return render_template('documentation.html', title='Documentation')
 
-@app.route('/api/words/<letter>')
-def letter():
-    pass
-
 @app.route('/api/search/<word>')
 def word_search(word):
     dictionary_result = Wordbank.get_word(word)
@@ -133,6 +157,7 @@ def synonyms(word):
     synonym = Wordbank.get_synonyms(word)
     return synonym
 
-
 if __name__ == '__main__':
+    #scheduler.add_job(Wordbank.send_word, 'cron', hour=10, minute=00, name='Word of the Day')
+    #scheduler.start()
     app.run(debug=True)
